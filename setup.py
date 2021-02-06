f9e4904578ba31b9e3625378ebc218e144dbfaa4
Stephen Mott <stephen@motts.id.au>
2021-02-07 01:11:40 +0800

    initial commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='SModelWrap',
+    py_modules=[
+        'SModelWrap'
+    ],
+    packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    version='1.0.0',
+    description='Quick wrapper to get predictions to work with AWS Sagemaker',
+    author='Stephen Mott',
+    author_email="stephen@mott.email",
+    python_requires=">=3.6",
+    install_requires=["pandas", "numpy"]
+)
